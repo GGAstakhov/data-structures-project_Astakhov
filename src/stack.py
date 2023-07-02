25cfc86f25ad90b2496a7008949f59d7ee6d630e
George Astakhov <goha1115@mail.ru>
2023-07-02 18:26:50 +0300

    changes in homework-2 stack.py

--- src/stack.py
+++ src/stack.py
@@ -33,4 +33,9 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.top:
+            data = self.top.data
+            self.top = self.top.next_node
+            return data
+        else:
+            return None
